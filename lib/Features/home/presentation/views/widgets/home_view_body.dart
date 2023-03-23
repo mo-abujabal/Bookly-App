473435977082891916592057ac49587bd4a91d81
mo-abujabal <mohamed01279055834@gmail.com>
2023-03-23 16:55:11 +0200

    best seller list view

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -5,6 +5,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../../core/utils/assets.dart';
 import '../../../../../core/utils/styles.dart';
+import 'best_seller_list_view.dart';
 import 'best_seller_list_view_item.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view_item.dart';
@@ -15,26 +16,32 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: const [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-        SizedBox(
-          height: 30,
+    return CustomScrollView(slivers: [
+      SliverToBoxAdapter(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            CustomAppBar(),
+            FeaturedBooksListView(),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 30,
+                vertical: 16,
+              ),
+              child: Text(
+                'Best Seller',
+                style: Styles.textStyle18,
+              ),
+            ),
+          ],
         ),
-        Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 30,
-            vertical: 16,
-          ),
-          child: Text(
-            'Best Seller',
-            style: Styles.textStyle18,
-          ),
-        ),
-        BestSellerListViewItem(),
-      ],
-    );
+      ),
+      const SliverFillRemaining(
+        child: BestSellerListView(),
+      )
+    ]);
   }
 }
